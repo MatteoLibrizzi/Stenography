0f59e8ce7ce7dc492097c781d98b1ac5100e8090
Matteo-unroot <matteo-unroot@matteosterminal.station>
2020-10-04 18:07:14 +0200

    xxx

--- Hide.py
+++ Hide.py
@@ -1,5 +1,6 @@
 from PIL import Image as im
 import math
+
 #FOR SOME REASON SPLIT MSG DOESN'T REPORT CORRECTLY THE VALUES OF THE BITS
 class Image:#image obj
 	def __init__(self,path):
@@ -31,15 +32,21 @@ class Messagge:
 		j=0#index for the returned list (incremented hald the times)
 		a=0#index for the input list
 
-		nOfGroups=int(math.ceil(len(self.bit)/2))#numer of groups by two
+		if(len(self.bit)%2==0):#checks if numbers of bits is odd
+			nOfGroups=int(len(self.bit)/2)#if not everything is fine
+			earlyStop=False
+		else:
+			nOfGroups=int(math.ceil(len(self.bit)/2))#if it is, it is rounded up, as to not go out of index later
+			earlyStop=True
 		k=0
-		for a in range(nOfGroups):#loops through the list to fill without exceeding it
+		while a<nOfGroups*2:#loops through the list to fill without exceeding it
 			slots.append([])#appends a new list (gonna be filled with 2 elements)
 			for i in range(2):
-				slots[j].append(self.bit[a])#appends at the end of the newly created list a new element from self bit
-				print(self.bit[a])
+
+				if(not(earlyStop and (a==(nOfGroups*2)-1))):#if it is odd AND it is in the last iterarion it does nothing, otherwise appends a new bit
+
+					slots[j].append(self.bit[a])#appends at the end of the newly created list a new element from self bit
 				a+=1#the element from self bit changes everytime where as the element from slots every two
-				
 			j+=1
 		print(slots)
 		return slots#return the bits grouped in twos
@@ -49,7 +56,7 @@ class Messagge:
 
 
 print("Type in a messagge to hide with only ascii: ")
-m="UUUU"
+m="AAAAAAAAAAAAA"
 print("Type in the path for the image to hide it in: ")
 path="image1.jpg"
 
